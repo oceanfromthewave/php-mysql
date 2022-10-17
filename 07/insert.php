256dccb6a1ec7512d0e4f218151cd77d468159a7
oceanfromthewave <oceanfromthewave@gmail.com>
2022-10-17 17:13:29 +0900

    Initial commit

---
+++ 07/insert.php
@@ -0,0 +1,17 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "koreait", "opentutorls");
+    $sql = "
+        INSERT INTO topic (
+        title,
+        description,
+        created
+        ) VALUES (
+            'MySQL',
+            'MySQL is ....',
+            NOW()
+        )";
+    $result = mysqli_query($conn, $sql);
+    if( $result == false ) {
+        echo mysqli_error($conn);
+    }
+?>
